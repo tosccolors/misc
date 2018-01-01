ad2635cd16bb1ef5da66f83dcc6bcdbc3bfb3324
Willem Hulshof <w.hulshof@magnus.nl>
2018-01-01 16:49:39 +0100

    __init__ omission

--- batch_vendor_invoice_import/models/__init__.py
+++ batch_vendor_invoice_import/models/__init__.py
@@ -3,6 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import account_invoice_import
-#from . import account_invoice_import_config
+from . import account_invoice_import_config
 from . import attachment
 from . import task
