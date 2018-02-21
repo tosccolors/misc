e0ee6ad4bcf5df3fa1f179093fc8602edbca64f9
Willem Hulshof <w.hulshof@magnus.nl>
2018-02-21 23:26:18 +0100

    errortje!

--- account_invoice_2step_validation/models/account_invoice.py
+++ account_invoice_2step_validation/models/account_invoice.py
@@ -117,7 +117,7 @@ class Invoice(models.Model):
     def _write(self, vals):
         pre_not_reconciled = self.filtered(lambda invoice: not invoice.reconciled)
         pre_reconciled = self - pre_not_reconciled
-        res = super(AccountInvoice, self)._write(vals)
+        res = super(Invoice, self)._write(vals)
         reconciled = self.filtered(lambda invoice: invoice.reconciled)
         not_reconciled = self - reconciled
         (reconciled & pre_reconciled).filtered(lambda invoice: invoice.state in ['auth','verified']).action_invoice_paid()
