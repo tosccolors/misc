64ffe0efe2b134502737c40304cc1f7c1a66d68f
Willem Hulshof <w.hulshof@magnus.nl>
2018-05-05 14:30:53 +0200

    import '_' to make error messaging translatable in task.py

--- batch_vendor_invoice_import/models/task.py
+++ batch_vendor_invoice_import/models/task.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import odoo
-from odoo import models, fields, api
+from odoo import models, fields, api, _
 from odoo import tools
 from odoo.exceptions import ValidationError
 from unidecode import unidecode
