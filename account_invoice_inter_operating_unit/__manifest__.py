43b3ec62bee4c30f3462a9d29bbde1c97730e1de
Willem Hulshof <w.hulshof@magnus.nl>
2018-05-12 17:52:47 +0200

    first account_inter_OU_invoice

---
+++ account_invoice_inter_operating_unit/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# © 2013-2014 Odoo SA
+# © 2015-2017 Chafique Delli <chafique.delli@akretion.com>
+# © 2018 Willem Hulshof <w.hulshof@magnus.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Inter Company/Operating-Unit Module for Invoices',
+    'summary': 'Intercompany Operating Unit invoice rules',
+    'version': '10.0.1.1.0',
+    'category': 'Accounting & Finance',
+    'website': 'http://www.magnus.nl',
+    'author': 'Odoo SA, Akretion, Magnus, Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'depends': [
+        'account_accountant',
+        'onchange_helper',
+        'account_operating_unit',
+
+    ],
+    'data': [
+        'views/operating_unit_view.xml',
+#        'views/res_config_view.xml',
+    ],
+    'demo': [
+    ],
+}
