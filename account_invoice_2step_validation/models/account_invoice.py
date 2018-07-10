bd3ee96b20d9925571107a72c9f4e5e95e5a729e
Willem Hulshof <w.hulshof@magnus.nl>
2018-07-10 16:38:58 +0200

    clean chain with super for _prepare_move_line_domain(self) and override with right staate for create_account_payment_line()

--- account_invoice_2step_validation/models/account_invoice.py
+++ account_invoice_2step_validation/models/account_invoice.py
@@ -195,12 +195,78 @@ class Invoice(models.Model):
             raise UserError(_("Invoice must be in draft, Start Workflow, Pro-forma, open or Authorized state in order to be cancelled."))
         return self.action_cancel()
 
+    #Overridden:
+    @api.multi
+    def create_account_payment_line(self):
+        apoo = self.env['account.payment.order']
+        aplo = self.env['account.payment.line']
+        result_payorder_ids = []
+        action_payment_type = 'debit'
+        for inv in self:
+            if inv.state != 'verified' and not (inv.state == 'auth' and inv.verif_tresh_exceeded == False):
+                raise UserError(_(
+                    "The invoice %s is not in auth or verified state") % inv.number)
+            if not inv.payment_mode_id:
+                raise UserError(_(
+                    "No Payment Mode on invoice %s") % inv.number)
+            if not inv.move_id:
+                raise UserError(_(
+                    "No Journal Entry on invoice %s") % inv.number)
+            if not inv.payment_order_ok:
+                raise UserError(_(
+                    "The invoice %s has a payment mode '%s' "
+                    "which is not selectable in payment orders."))
+            payorders = apoo.search([
+                ('payment_mode_id', '=', inv.payment_mode_id.id),
+                ('state', '=', 'draft')])
+            if payorders:
+                payorder = payorders[0]
+                new_payorder = False
+            else:
+                payorder = apoo.create(inv._prepare_new_payment_order())
+                new_payorder = True
+            result_payorder_ids.append(payorder.id)
+            action_payment_type = payorder.payment_type
+            count = 0
+            for line in inv.move_id.line_ids:
+                if line.account_id == inv.account_id and not line.reconciled:
+                    paylines = aplo.search([
+                        ('move_line_id', '=', line.id),
+                        ('state', '!=', 'cancel')])
+                    if not paylines:
+                        line.create_payment_line_from_move_line(payorder)
+                        count += 1
+            if count:
+                if new_payorder:
+                    inv.message_post(_(
+                        '%d payment lines added to the new draft payment '
+                        'order %s which has been automatically created.')
+                                     % (count, payorder.name))
+                else:
+                    inv.message_post(_(
+                        '%d payment lines added to the existing draft '
+                        'payment order %s.')
+                                     % (count, payorder.name))
+            else:
+                raise UserError(_(
+                    'No Payment Line created for invoice %s because '
+                    'it already exists or because this invoice is '
+                    'already paid.') % inv.number)
+        action = self.env['ir.actions.act_window'].for_xml_id(
+            'account_payment_order',
+            'account_payment_order_%s_action' % action_payment_type)
+        if len(result_payorder_ids) == 1:
+            action.update({
+                'view_mode': 'form,tree,pivot,graph',
+                'res_id': payorder.id,
+                'views': False,
+            })
+        else:
+            action.update({
+                'view_mode': 'tree,form,pivot,graph',
+                'domain': "[('id', 'in', %s)]" % result_payorder_ids,
+                'views': False,
+            })
+        return action
 
-# TODO: FIXME: Expense-Move relation no longer exists
-# -- deep
-# Brought from NSM_Expense
-# domain in payment_order to filter Verfied Invoice
-# class Move(models.Model):
-#     _inherit = ['account.move']
-#
-#     expense_id = fields.One2many('hr.expense', 'account_move_id', 'Expense', readonly=True)
+
--- account_invoice_2step_validation/wizard/payment_order_create.py
+++ account_invoice_2step_validation/wizard/payment_order_create.py
@@ -33,79 +33,12 @@ class AccountPaymentLineCreate(models.TransientModel):
     # Overridden:
     @api.multi
     def _prepare_move_line_domain(self):
-        self.ensure_one()
-        journals = self.journal_ids or self.env['account.journal'].search([])
-        domain = [('reconciled', '=', False),
-                  ('company_id', '=', self.order_id.company_id.id),
-                  ('journal_id', 'in', journals.ids),
+        domain = super(AccountPaymentLineCreate, self)._prepare_move_line_domain()
 
-                  # ('invoice_id.state', '=', 'auth')
-
-                  '|',
-                  # '|',
-                  # ('move_id.expense_id.state', '=', 'accepted'), # Expense-Move relation no longer exists
+        domain += ['|',
                   ('invoice_id.state', '=', 'verified'),
                   '&',
                   ('invoice_id.state', '=', 'auth'),('invoice_id.verif_tresh_exceeded','=', False),
-
                   ]
-
-        if self.target_move == 'posted':
-            domain += [('move_id.state', '=', 'posted')]
-        if not self.allow_blocked:
-            domain += [('blocked', '!=', True)]
-        if self.date_type == 'due':
-            domain += [
-                '|',
-                ('date_maturity', '<=', self.due_date),
-                ('date_maturity', '=', False)]
-        elif self.date_type == 'move':
-            domain.append(('date', '<=', self.move_date))
-        if self.invoice:
-            domain.append(('invoice_id', '!=', False))
-        if self.payment_mode:
-            if self.payment_mode == 'same':
-                domain.append(
-                    ('payment_mode_id', '=', self.order_id.payment_mode_id.id))
-            elif self.payment_mode == 'same_or_null':
-                domain += [
-                    '|',
-                    ('payment_mode_id', '=', False),
-                    ('payment_mode_id', '=', self.order_id.payment_mode_id.id)]
-
-        if self.order_id.payment_type == 'outbound':
-            # For payables, propose all unreconciled credit lines,
-            # including partially reconciled ones.
-            # If they are partially reconciled with a supplier refund,
-            # the residual will be added to the payment order.
-            #
-            # For receivables, propose all unreconciled credit lines.
-            # (ie customer refunds): they can be refunded with a payment.
-            # Do not propose partially reconciled credit lines,
-            # as they are deducted from a customer invoice, and
-            # will not be refunded with a payment.
-            domain += [
-                ('credit', '>', 0),
-                #  '|',
-                ('account_id.internal_type', '=', 'payable'),
-                #  '&',
-                #  ('account_id.internal_type', '=', 'receivable'),
-                #  ('reconcile_partial_id', '=', False),  # TODO uncomment
-            ]
-        elif self.order_id.payment_type == 'inbound':
-            domain += [
-                ('debit', '>', 0),
-                ('account_id.internal_type', '=', 'receivable'),
-                ]
-        # Exclude lines that are already in a non-cancelled
-        # and non-uploaded payment order; lines that are in a
-        # uploaded payment order are proposed if they are not reconciled,
-        paylines = self.env['account.payment.line'].search([
-            ('state', 'in', ('draft', 'open', 'generated')),
-            ('move_line_id', '!=', False)])
-        if paylines:
-            move_lines_ids = [payline.move_line_id.id for payline in paylines]
-            domain += [('id', 'not in', move_lines_ids)]
-
         return domain
 
