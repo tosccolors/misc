405027c15707359afa70dc7bfe43c300bea86d6f
Willem Hulshof <w.hulshof@magnus.nl>
2019-01-03 20:16:25 +0100

    first commit keyportal mod

--- keyportal_signup/__init__.py
+++ keyportal_signup/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright 2014 Eurogroup Consulting
+#    Copyright 2019 Magnus
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -18,5 +18,5 @@
 #
 ##############################################################################
 
-import res_users
+import res_partner
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
