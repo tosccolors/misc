3871bc2c14e60bd6f8d427c7028375ed63631892
Dhrupesh <dhrupesh@ip-192-168-0-101.eu-west-1.compute.internal>
2019-01-21 19:13:46 +0530

    Added validation on date start and end.

--- contract_registry/models/contract_period.py
+++ contract_registry/models/contract_period.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, RedirectWarning, ValidationError
+from openerp.tools.translate import _
 
 class ContractPeriod(models.Model):
      _name = 'contract.period'
@@ -11,3 +13,12 @@ class ContractPeriod(models.Model):
      )
      
      period_id = fields.Many2one("contract.registry",string="Period")
+     
+     @api.multi
+     @api.constrains('date_end', 'date_start')
+     def date_constrains(self):
+         for rec in self:
+             if rec.date_end < rec.date_start:
+                 raise ValidationError(_('Sorry, End Date Must be greater Than Start Date...'))
+     
+     
