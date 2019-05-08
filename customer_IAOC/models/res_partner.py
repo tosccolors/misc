5097f3135e1605996cc16cf1f8f959f5121b4edb
kumarinie <kumarisushmanie@gmail.com>
2019-05-08 14:17:43 +0530

    Fix: vat & customer_invoice_transmit_method_id fiew Module: customer_IAOC

--- customer_IAOC/models/res_partner.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-from odoo import api, models, fields,_
-class ResPartner(models.Model):
-    _inherit = 'res.partner'
-    
-    vat = fields.Char("TIN")
