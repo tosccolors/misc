4622257c263e99200ff05ecacf9ab3af9f4b9792
Dhrupesh <kotadiyadhrupesh@gmail.com>
2019-08-05 16:58:41 +0530

    T0339 - Invoice Email Import function

---
+++ fetchmail_invoice2data/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Fetch mail and create invoice",
+
+    'summary': """
+            This modules will help to create invoice from mail.
+    """,
+    'description': """
+    """,
+        'author': "Magnus - DK",
+        'website': "http://www.yourcompany.com",
+        'category': 'Others',
+        'version': '0.1',
+        'depends': ['mail'],
+        'data': [],
+        'demo': [],
+    }
