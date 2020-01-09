37573a29693eed1406e245f0e00977bcf78cc653
praveen-kumarG <praveenkg@systemsvalley.co.in>
2020-01-09 19:52:36 +0530

    BACK PORT OCA/connector-jira repository from v11 to v10

---
+++ connector_jira/wizards/__init__.py
@@ -0,0 +1,4 @@
+from . import jira_backend_auth
+from . import jira_account_analytic_line_import
+# Commented the file as the inherited model (project.task.merge.wizard) is not available anywhere in the code
+# from . import project_task_merge_wizard
---
+++ multi_step_wizard/__manifest__.py
@@ -0,0 +1,13 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{'name': 'Multi-Steps Wizards',
+ 'version': '11.0.1.1.0',
+ 'author': 'Camptocamp,Odoo Community Association (OCA)',
+ 'license': 'AGPL-3',
+ 'depends': ['base'],
+ 'website': 'https://github.com/OCA/connector-jira',
+ 'data': [
+     'views/multi_step_wizard_views.xml',
+     ],
+ 'installable': True,
+ }
