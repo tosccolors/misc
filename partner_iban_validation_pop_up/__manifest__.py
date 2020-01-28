90224c1b5c4034eef97718a7bd15e4398184af5a
kumarinie <kumarisushmanie@gmail.com>
2020-01-28 17:42:15 +0530

    Task: T0370 - Partner IBAN validation message Module: partner_iban_validation_pop_up

--- partner_iban_validation_pop_up/__manifest__.py
+++ partner_iban_validation_pop_up/__manifest__.py
@@ -25,7 +25,6 @@
     'data': [
         # 'security/ir.model.access.csv',
         # 'views/views.xml',
-        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
