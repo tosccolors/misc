44ba7f61d46682b86e36ff8942f95604f66955c2
kumarinie <kuamrisushmanie@gmail.com>
2021-05-04 17:16:26 +0530

    Updated: account_reversal_sql_jq

--- account_reversal_sql_jq/__manifest__.py
+++ account_reversal_sql_jq/__manifest__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Account Reversal Via Sql",
+    'name': "Account Reversal Sql/Job Queue",
 
     'summary': """
-         perform the reversal via a SQL query""",
+         perform the reversal via a SQL query OR Job Queue""",
 
     'description': """
        \n Create a new boolean field 'reversal via SQL' in the configuration form view.
@@ -27,11 +27,11 @@
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/account_views.xml',
+        # 'views/templates.xml',
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
 }
--- account_reversal_sql_jq/models/account_config_settings.py
+++ account_reversal_sql_jq/models/account_config_settings.py
@@ -4,4 +4,5 @@ class AccountConfigSettings(models.TransientModel):
     
     _inherit = 'account.config.settings'
 
-    reversal_via_sql = fields.Boolean(string="Reversal Via Sql", related='company_id.reversal_via_sql')   
+    reversal_via_sql = fields.Boolean(string="Reversal Via Sql", related='company_id.reversal_via_sql')
+    perform_reversal_by_line_jq = fields.Boolean(string="Perform Reversal by Line Job Queue", related='company_id.perform_reversal_by_line_jq')
