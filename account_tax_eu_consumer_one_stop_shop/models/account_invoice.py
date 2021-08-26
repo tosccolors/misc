84b0edc3b4359cd0494895a33d9e40ac6448726c
kumarinie <kuamrisushmanie@gmail.com>
2021-08-26 17:34:56 +0530

    Add country_id domain for line taxes Module:account_tax_eu_consumer_one_stop_shop

--- account_tax_eu_consumer_one_stop_shop/models/account_invoice.py
+++ account_tax_eu_consumer_one_stop_shop/models/account_invoice.py
@@ -7,13 +7,26 @@ class AccountInvoiceLine(models.Model):
     _inherit = "account.invoice.line"
 
     @api.onchange('product_id')
-    def _onchange_product_id(self):
-        result = super(AccountInvoiceLine(), self)._onchange_product_id()
-        if not self.product_id \
-                and self.invoice_id.fiscal_position_id.country_tax \
-                and type not in ('in_invoice', 'in_refund'):
-            result['domain']['invoice_line_tax_ids'] = [('country_id','=',self.invoice_id.partner_id.country_id.id)]
-        return result
+    def product_id_onchange(self):
+        data = {'invoice_line_tax_ids': [('company_id', '=', self.invoice_id.company_id.id)]}
+        if self.invoice_id.type in ('in_invoice', 'in_refund'):
+            type_tax_use = 'purchase'
+        if self.invoice_id.type in ('out_invoice', 'out_refund'):
+            type_tax_use = 'sale'
+            if not self.product_id \
+                    and self.invoice_id.fiscal_position_id.country_tax:
+                data['invoice_line_tax_ids'] += [('country_id', '=', self.invoice_id.partner_id.country_id.id)]
+        data['invoice_line_tax_ids'] += [('type_tax_use', '=', type_tax_use)]
+        return {'domain': data}
+
+    # @api.onchange('product_id')
+    # def _onchange_product_id(self):
+    #     result = super(AccountInvoiceLine(), self)._onchange_product_id()
+    #     if not self.product_id \
+    #             and self.invoice_id.fiscal_position_id.country_tax \
+    #             and type not in ('in_invoice', 'in_refund'):
+    #         result['domain']['invoice_line_tax_ids'] = [('country_id','=',self.invoice_id.partner_id.country_id.id)]
+    #     return result
 
     # @api.onchange('account_id')
     # def _onchange_account_id(self):
