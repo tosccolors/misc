6121c84f2e859469cb42385c10058bc6c6bad73c
Willem Hulshof <w.hulshof@magnus.nl>
2021-11-03 13:11:21 +0100

    haakjes

--- account_tax_eu_consumer_one_stop_shop/models/sale.py
+++ account_tax_eu_consumer_one_stop_shop/models/sale.py
@@ -9,7 +9,7 @@ class SaleOrderLine(models.Model):
     @api.multi
     @api.onchange('product_id')
     def product_id_change(self):
-        result = super(SaleOrderLine(), self).product_id_change()
+        result = super(SaleOrderLine, self).product_id_change()
         if not self.product_id \
             and self.order_id.fiscal_position_id.country_tax:
             result['domain']['tax_id'] = [('country_id', '=', self.order_id.partner_id.country_id.id)]
