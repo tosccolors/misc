0562c614121b54a30420400d08fd18a3fc91e077
Willem Hulshof <w.hulshof@magnus.nl>
2022-04-08 17:44:49 +0200

    delete bi_sql_excel

--- bi_sql_excel_reports/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
-from . import bi_sql_excel_report
-from . import bi_sql_excel_report_field
-from . import bi_sql_view
-
-
