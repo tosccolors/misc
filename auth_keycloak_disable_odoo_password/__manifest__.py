f75fdb0e33886600ad29e78d5088940ca9c3a1b2
Deepa Venkatesh <deepa4lok@yahoo.com>
2022-05-24 13:59:11 +0530

    App: auth_keycloak_disable_odoo_password

---
+++ auth_keycloak_disable_odoo_password/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    "name": "Odoo Disable Password",
+    "summary": "Disable Password in Login Page",
+    "version": "10.0.0.0.0",
+    'category': 'Tools',
+    'website' : "https://www.magnus.nl/",
+    'author': 'Magnus Red',
+    "depends": [
+        "auth_keycloak",
+    ],
+    "data": [
+        "views/res_users.xml"
+    ],
+}
