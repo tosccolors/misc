dc0baa87b6f7b52fc25efa01fe0129d9c90d10ba
kumarinie <kuamrisushmanie@gmail.com>
2022-09-23 12:17:34 +0530

    fix mapped one payment order

--- invoice_payment_order_check/models/invoice.py
+++ invoice_payment_order_check/models/invoice.py
@@ -10,8 +10,9 @@ class AccountInvoice(models.Model):
     def _compute_payment_order(self):
         payment_order_line = self.env['account.payment.line']
         for inv in self.filtered(lambda n: n.type == 'in_invoice'):
-            pol = payment_order_line.search([('move_line_id', 'in', inv.move_id.line_ids.ids)], limit=1)
-            inv.invoice_payment_order_id = pol.order_id.id
+            pol = payment_order_line.search([('move_line_id', 'in', inv.move_id.line_ids.ids)])
+            payment_order = pol.mapped('order_id')
+            inv.invoice_payment_order_id = payment_order and payment_order.ids[0] or payment_order
 
     invoice_payment_order_id = fields.Many2one('account.payment.order', 'Payment Order',
                                        compute="_compute_payment_order", store=True)
