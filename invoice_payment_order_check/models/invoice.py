623742c5767cc18fe27cb82bd287172935ec4a90
kumarinie <kuamrisushmanie@gmail.com>
2022-09-23 11:58:28 +0530

    fix: Added limit

--- invoice_payment_order_check/models/invoice.py
+++ invoice_payment_order_check/models/invoice.py
@@ -10,7 +10,7 @@ class AccountInvoice(models.Model):
     def _compute_payment_order(self):
         payment_order_line = self.env['account.payment.line']
         for inv in self.filtered(lambda n: n.type == 'in_invoice'):
-            pol = payment_order_line.search([('move_line_id', 'in', inv.move_id.line_ids.ids)])
+            pol = payment_order_line.search([('move_line_id', 'in', inv.move_id.line_ids.ids)], limit=1)
             inv.invoice_payment_order_id = pol.order_id.id
 
     invoice_payment_order_id = fields.Many2one('account.payment.order', 'Payment Order',
