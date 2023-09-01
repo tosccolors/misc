84e30ed0852336aac2355537d5674e393cbd7c66
Deepa Venkatesh <deepa4lok@yahoo.com>
2023-09-01 21:10:40 +0530

    Keycloak: Auto-push License

--- auth_keycloak_autopush_users/__manifest__.py
+++ auth_keycloak_autopush_users/__manifest__.py
@@ -4,10 +4,11 @@
 {
     "name": "Auto-Push Users to Keycloak",
     "summary": "Auto-push Portal user to Keycloak",
-    "version": "16.0.0.0",
+    "version": "16.0.1.0",
     'category': 'Tools',
     'website' : "https://www.tosc.nl/",
     "author": "Deepa, " "The Open Source company (TOSC)",
+    "license": "LGPL-3",
     "depends": [
         "auth_signup",
         "auth_keycloak",
