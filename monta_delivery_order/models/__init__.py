221f90167412052e9e2a4d279b5072313fee0a1b
kumarinie <kuamrisushmanie@gmail.com>
2024-01-05 18:02:18 +0530

    Rearrange the order

--- monta_delivery_order/models/__init__.py
+++ monta_delivery_order/models/__init__.py
@@ -5,6 +5,6 @@ from . import monta_picking
 from . import stock
 from . import purchase
 from . import monta_stock_lot
+from . import monta_delivery_block
 from . import sale
 from . import delivery_carrier
-from . import monta_delivery_block
