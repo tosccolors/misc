6d9ca15903a877ce6962aeb114d89e611df98944
Deepa Venkatesh <deepa4lok@yahoo.com>
2025-04-24 12:28:39 +0530

    [776]: Fixes [1]

--- monta_delivery_order/__manifest__.py
+++ monta_delivery_order/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/10.0/odoo/addons/base/module/module_data.xml
     # for the full list
     'category': 'Stock',
-    'version': '16.0.3.6',
+    'version': '16.0.3.7',
 
     # any module necessary for this one to work correctly
     'depends': ['sale_stock', 'partner_firstname', 'purchase_stock', 'base_address_extended',
