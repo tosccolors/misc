73ff6904829896f6817d5b523a1a4ce245a7dd30
Holger Brunn <mail@hunki-enterprises.com>
2025-05-01 10:45:42 +0200

    [ADD] #752 project_tosc

---
+++ project_tosc/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': "Project (TOSC customizations)",
+    'summary': "TOSC's customizations for the project module and related modules",
+    'author': "The Open Source Company (TOSC)",
+    'website': "https://tosc.nl",
+    'category': 'Tools',
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    'depends': ['project', 'helpdesk_mgmt'],
+    'data': [
+        'views/project_task.xml',
+        'views/helpdesk_ticket.xml',
+    ],
+}
