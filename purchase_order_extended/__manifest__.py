74d084e35ebf8db66d8f466c7df5d77746c933e0
Deepa Venkatesh <deepa4lok@yahoo.com>
2025-05-22 13:57:45 +0530

    [784] UI finetunes

--- purchase_order_extended/__manifest__.py
+++ purchase_order_extended/__manifest__.py
@@ -16,7 +16,7 @@
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Inventory/Purchase',
-    'version': '16.0.3.0.0',
+    'version': '16.0.4.0.0',
 
     # any module necessary for this one to work correctly
     'depends': ['purchase_stock', 'stock', 'stock_account'],
