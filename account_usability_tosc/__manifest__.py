e845ff661631f212f8a0cb482cef9f2873cb4113
Holger Brunn <mail@hunki-enterprises.com>
2025-07-03 07:26:04 +0200

    [MIG] backport account_usability_tosc

--- account_usability_tosc/__manifest__.py
+++ account_usability_tosc/__manifest__.py
@@ -1,14 +1,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Account Usability (TOSC)",
-    "version": "16.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "summary": "Adapt account_usability_akretion for TOSC",
     "author": "The Open Source Company",
     "website": "http://www.tosc.nl",
     "category": "Accounting & Finance",
     "depends": [
-        "account_usability_akretion",
+        "account_usability",
     ],
     "installable": True,
     "data": [
