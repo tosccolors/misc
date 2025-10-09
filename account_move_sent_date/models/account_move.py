347e486be9c80b67af115b9a1fddfb7a8962137d
Holger Brunn <mail@hunki-enterprises.com>
2025-10-09 13:55:44 +0200

    [FIX] #1223 account_move_sent_date: don't copy sent_date
    when copying invoices

--- account_move_sent_date/models/account_move.py
+++ account_move_sent_date/models/account_move.py
@@ -4,7 +4,7 @@ from odoo import api, fields, models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    sent_date = fields.Date()
+    sent_date = fields.Date(copy=False)
 
     def write(self, vals):
         if vals.get('is_move_sent') and 'sent_date' not in vals:
