f328386982cb2a982bca1ba56fe64ca42a2f13f7
Holger Brunn <mail@hunki-enterprises.com>
2026-02-13 11:00:55 +0100

    [FIX] #1471 mail_delivery_error_popup: better detection of sending error

--- mail_delivery_error_popup/models/mail_mail.py
+++ mail_delivery_error_popup/models/mail_mail.py
@@ -4,11 +4,10 @@ from odoo import _, api, fields, models
 class MailMail(models.Model):
     _inherit = "mail.mail"
 
-    def write(self, vals):
-        result = super().write(vals)
-        if vals.get('state') == 'exception':
+    def _postprocess_sent_message(self, success_pids, failure_reason=False, failure_type=None):
+        if failure_reason or failure_type:
             self._mail_delivery_error_popup()
-        return result
+        return super()._postprocess_sent_message(success_pids, failure_reason=failure_reason, failure_type=failure_type)
 
     def _mail_delivery_error_popup(self):
         notify_group = self.env.ref('mail_delivery_error_popup.group_mail_delivery_error_popup')
