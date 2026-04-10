0c91e5e45363bde07964cabc638e4bd32bac2c10
Holger Brunn <mail@hunki-enterprises.com>
2026-04-10 15:54:52 +0200

    [ADD] base_tier_validation_reevaluate

---
+++ base_tier_validation_reevaluate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Tier validation: Reevaluate reviews",
+    'summary': "Allows to re-evaluate pending reviews in case underlying objects have changed",
+    'author': "The Open Source Company (TOSC)",
+    'website': "https://tosc.nl",
+    'category': 'Tools',
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    'depends': ['base_tier_validation'],
+    "data": [
+        'data/ir_actions_server.xml',
+    ],
+}
