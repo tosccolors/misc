07abb9dcf6ef57d85347d4d59d7839c7d43fd5a9
Holger Brunn <mail@hunki-enterprises.com>
2026-04-17 07:20:25 +0200

    [ADD] base_tier_validation_expand_reviews

---
+++ base_tier_validation_expand_reviews/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Tier validation: Expand reviews dropdown by default",
+    'summary': "Show the reviews dropdown in open state by default",
+    'author': "The Open Source Company (TOSC)",
+    'website': "https://tosc.nl",
+    'category': 'Tools',
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    'depends': ['base_tier_validation'],
+    "qweb": [
+        "static/src/xml/tier_review_template.xml",
+    ],
+}
